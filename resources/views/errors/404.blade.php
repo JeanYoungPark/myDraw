6490bea6f5b5fdc0be9aa63a6cf2214abab89998
JeanYoungPark <Jean.Young.Park.96@gmail.com>
2022-01-17 14:31:21 +0900

    404페이지 수정

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,5 +1,5 @@
-<div id="errors" style="position:fixed; width:100%; height:100%;">
-    <div style="position:absolute; top:25%; left:0; right:0; margin:0 auto; width:100px; height:auto;"><img src="{{ asset('images/logo_s.jpg') }}" alt="" style="width:100%;"></div>
-    <h2 style="position:absolute; width:100%; top:40%; text-align:center; color:#333; font-weight:normal;">Sorry, the page you are looking for could not be found.</h2>
-    <a href="/" style="position:absolute; display:block; width:200px; height:30px; top:48%; left:0; right:0; margin:0 auto; text-align:center; line-height:30px;color:#333;text-decoration:none; background:#f0f0f0; border:1px solid #ccc">home</a>
+<div id="errors" style="display:flex; flex-direction:column; align-items:center;justify-content:center; position:fixed; width:100%; height:100%;">
+    <div style="width:100px; height:auto;"><img src="{{ asset('images/logo_s.jpg') }}" alt="" style="width:100%;"></div>
+    <h2 style="width:100%;text-align:center; color:#333; font-weight:normal;">Sorry, the page you are looking for could not be found.</h2>
+    <a href="/" style="pdisplay:block; width:200px; height:30px;text-align:center; line-height:30px;color:#333;text-decoration:none; background:#f0f0f0; border:1px solid #ccc">home</a>
 </div>
